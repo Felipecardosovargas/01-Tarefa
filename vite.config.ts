456a72750126ca1294859e48173b85be689e4327
Felipe Cardoso Vargas <128812730+Felipecardosovargas@users.noreply.github.com>
2025-02-24 04:23:15 -0300

    configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<01-tarefa>/',
+  base: "/01-tarefa",
   plugins: [react()],
   server: {
     hmr: true, // Habilita o Hot Module Replacement (HMR)
